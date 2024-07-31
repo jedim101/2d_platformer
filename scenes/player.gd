bf3ae98215dd1cd8af2d67b5c53305439c29128a
matthewglasser <matthewsglasser@gmail.com>
2024-07-30 22:19:15 -0400

    box

--- scenes/player.gd
+++ scenes/player.gd
@@ -26,3 +26,8 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * 100)
