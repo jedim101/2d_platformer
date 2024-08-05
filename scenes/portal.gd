7a63e5225934cfd5f1dbdf9296488e21f4c93071
matthewglasser <matthewsglasser@gmail.com>
2024-08-04 22:46:04 -0400

    scalable portals

--- scenes/portal.gd
+++ scenes/portal.gd
@@ -8,12 +8,22 @@ func _on_body_entered(body):
 
 	for portal in get_tree().get_nodes_in_group("portal"):
 		if self != portal:
-			if portal.disabled:
+			if portal.disabled or scale.y < body.scale.y:
 				break
 
 			disabled = true
 
-			body.global_position = portal.global_position - (global_position - body.global_position)
+			var scale_factor = portal.scale / scale
+
+			body.global_position = portal.global_position - (global_position - body.global_position) * scale_factor
+
+			# body.global_position += ( portal.global_position - body.global_position) * scale_factor
+			
+			# # b + (p - b) * s
+
+			# p - (c - b)
+
+			body.scale *= scale_factor
 
 			await get_tree().create_timer(0.1).timeout
 			disabled = false
