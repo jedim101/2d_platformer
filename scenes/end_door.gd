3c81c399d6869485ecd24946b7a209342c92c204
matthewglasser <matthewsglasser@gmail.com>
2024-08-08 20:25:01 -0400

    level 7

--- scenes/end_door.gd
+++ scenes/end_door.gd
@@ -6,5 +6,3 @@ func _on_body_entered(body):
 		return
 	
 	get_tree().change_scene_to_file("res://scenes/level_" + str(int(get_tree().current_scene.name.right(1)) + 1) + ".tscn")
-
-	pass # Replace with function body.
