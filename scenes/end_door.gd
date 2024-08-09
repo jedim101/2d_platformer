e3485fd8363dfcb5d71acfd58487febca85010c3
matthewglasser <matthewsglasser@gmail.com>
2024-08-08 21:00:27 -0400

    only player activates door

--- scenes/end_door.gd
+++ scenes/end_door.gd
@@ -2,7 +2,5 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	if body is StaticBody2D:
-		return
-	
-	get_tree().change_scene_to_file("res://scenes/level_" + str(int(get_tree().current_scene.name.right(1)) + 1) + ".tscn")
+	if body is CharacterBody2D:
+		get_tree().change_scene_to_file("res://scenes/level_" + str(int(get_tree().current_scene.name.right(1)) + 1) + ".tscn")
