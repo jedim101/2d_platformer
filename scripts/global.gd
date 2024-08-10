26c711b64f2ac6f8b565748f3905b5a4532781f5
matthewglasser <matthewsglasser@gmail.com>
2024-08-10 16:17:14 -0400

    pause menu works

--- scripts/global.gd
+++ scripts/global.gd
@@ -2,3 +2,9 @@ extends Node
 
 var unlocked_level = 1
 var paused = false
+
+func _process(_delta):
+  if paused:
+    Engine.time_scale = 0
+  else:
+    Engine.time_scale = 1
--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,13 +1,31 @@
 extends Control
 
+func _ready():
+	if Global.paused:
+		show()
+	else:
+		hide()
+
+func control_menu():
+	Global.paused = !Global.paused
+
+	if Global.paused:
+		show()
+	else:
+		hide()
+
 func _process(_delta):
 	if Input.is_action_just_pressed("pause"):
-		Global.paused = !Global.paused
-
-		if Global.paused:
-			show()
-			Engine.time_scale = 0
-		else:
-			hide()
-			Engine.time_scale = 1
+		control_menu()
 		
+func _on_resume_pressed():
+	control_menu()
+
+
+func _on_restart_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	Global.paused = !Global.paused
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
