840353d4e41ad51a16d86a9a3ffd20def8ed52a5
matthewglasser <matthewsglasser@gmail.com>
2024-08-09 22:50:28 -0400

    fix door bug

--- scenes/end_door.gd
+++ scenes/end_door.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:
-		get_tree().change_scene_to_file("res://scenes/level_" + str(int(get_tree().current_scene.name.right(1)) + 1) + ".tscn")
+		get_tree().change_scene_to_file("res://scenes/level_" + str(int(get_tree().current_scene.name.substr(6)) + 1) + ".tscn")
