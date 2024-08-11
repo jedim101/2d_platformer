b65bfc9576920c29fbe24960c229bc22d5eff5b5
matthewglasser <matthewsglasser@gmail.com>
2024-08-11 08:39:11 -0400

    make player stronger

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
+const PUSH_FORCE = 50
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -39,4 +40,4 @@ func _physics_process(delta):
 	for i in get_slide_collision_count():
 		var c = get_slide_collision(i)
 		if c.get_collider() is RigidBody2D:
-			c.get_collider().apply_central_impulse(-c.get_normal() * 30)
+			c.get_collider().apply_central_impulse(-c.get_normal() * PUSH_FORCE)
