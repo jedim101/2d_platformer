12a70ff639d711bbff30c784e8bf97a6d6842a47
matthewglasser <matthewsglasser@gmail.com>
2024-08-21 14:29:39 +0200

    win screen

--- scripts/end_door.gd
+++ scripts/end_door.gd
@@ -5,5 +5,7 @@ func _on_body_entered(body):
 	if body is CharacterBody2D && body.global_scale.y <= global_scale.y:
 		var next_level = int(get_tree().current_scene.name.substr(6)) + 1
 		Global.unlocked_level = max(Global.unlocked_level, next_level)
+
+		var next_level_file = "res://scenes/level_" + str(next_level) + ".tscn"
 		
-		get_tree().change_scene_to_file("res://scenes/level_" + str(next_level) + ".tscn")
+		get_tree().change_scene_to_file(next_level_file if ResourceLoader.exists(next_level_file) else "res://scenes/you_win.tscn")
