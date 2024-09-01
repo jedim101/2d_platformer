c436eebaf56bc480e2707649ccfe9d0aaa16304f
matthewglasser <matthewsglasser@gmail.com>
2024-08-31 22:28:01 -0400

    decent jump/fall

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 const PUSH_FORCE = 50
 
-var pushing = false
+var animation = "walking"
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -18,10 +18,14 @@ func _physics_process(delta):
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	elif animation == "jumping":
+		animation = "walking"
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		$Sprite2D/FrameAnimator.play("jumping")
 		velocity.y = jump_velocity
+		animation = "jumping"
+		$Sprite2D/FrameAnimator.play(animation)
+
 
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
@@ -37,16 +41,22 @@ func _physics_process(delta):
 		$FrontArea.position.x = 0
 
 	if velocity.x != 0 and not $Sprite2D/FrameAnimator.is_playing() and is_on_floor():
-		$Sprite2D/FrameAnimator.play("pushing" if pushing else "walking")
+
+
+		$Sprite2D/FrameAnimator.play(animation)
 
 		$AudioStreamPlayer.volume_db = 80 * Global.sounds_volume -80
 
 		if Global.sounds_volume > 0:
 			$AudioStreamPlayer.play()
 
-	elif velocity.x == 0: 
+	elif animation == "walking" and (velocity.x == 0 or not is_on_floor()): 
 		$Sprite2D/FrameAnimator.stop()
 		$AudioStreamPlayer.stop()
+	
+	if not is_on_floor() and not animation == "jumping":
+		$Sprite2D.frame = 14
+		animation = "walking"
 
 	move_and_slide()
 
@@ -57,13 +67,11 @@ func _physics_process(delta):
 
 func _on_area_2d_body_entered(body:Node2D):
 	if body in get_tree().get_nodes_in_group("boxes"):
-		pushing = true
+		animation = "pushing"
 		$Sprite2D/FrameAnimator.stop()
 
 
 func _on_area_2d_body_exited(body:Node2D):
 	if body in get_tree().get_nodes_in_group("boxes"):
-		pushing = false
-		$Sprite2D/FrameAnimator.stop()
-		$Sprite2D/FrameAnimator.play("walking")
-		$Sprite2D/FrameAnimator.stop()
+		animation = "walking"
+		$Sprite2D/FrameAnimator.play("RESET")
